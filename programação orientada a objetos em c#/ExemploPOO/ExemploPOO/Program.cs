bbad932ca5763b87bb9ffce27ecaf045526cd31a
lanozubem <zubemluano@gmail.com>
2021-12-25 10:05:30 -0300

    Classes Seladas ou finais e classe Object

--- programação orientada a objetos em c#/ExemploPOO/ExemploPOO/Program.cs
+++ programação orientada a objetos em c#/ExemploPOO/ExemploPOO/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ExemploPOO.Models;
 
+// Abstração
 // Pessoa p1 = new Pessoa();
 // p1.Nome = "Neo";
 // p1.Idade = 35;
@@ -9,12 +10,14 @@
 // r.DefinirMedidas(0, 10);
 // System.Console.WriteLine(r.ObterArea());
 
+// Herança e Polimorfismo
 // Professor p = new Professor();
 // p.Nome = "Victor";
 // p.Idade = 42;
 // p.Apresentar();
 // p.Salario = 4877.99;
 
+// Herança e Polimorfismo
 // Aluno a = new Aluno();
 // a.Nome = "Flash";
 // a.Idade = 16;
@@ -22,10 +25,15 @@
 // a.Nota = 7;
 // a.Apresentar();
 
+// Sobrecarga de métodos
 // Calculadora c = new Calculadora();
 // System.Console.WriteLine(c.Somar(1,2));
 // System.Console.WriteLine(c.Somar(1, 2, 3));
 
-var c = new Corrente();
-c.Creditar(100);
-c.ExibirSaldo();
+// var c = new Corrente();
+// c.Creditar(100);
+// c.ExibirSaldo();
+
+// Classe Object
+// var comp = new Computador();
+// comp.ToString()
